67ec26dd4545d433b1d1e4e309e852dc8e199052
Aayush kr. Singh <111671884+aayushsing1@users.noreply.github.com>
2023-03-16 22:55:23 +0530

    Add files via upload

---
+++ linkshorteners.py
@@ -0,0 +1,11 @@
+import pyshorteners
+
+# create an instance of the Shortener class
+shortener = pyshorteners.Shortener()
+
+# get the short URL for a long URL
+long_url = 'https://converterio.pythonanywhere.com/'
+short_url = shortener.tinyurl.short(long_url)
+
+# print the short URL
+print(short_url)
